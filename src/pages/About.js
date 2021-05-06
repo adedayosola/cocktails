7138dafe79a41e50759239d2debc01363e5285dc
dayoadediran <gistwithdayo@yahoo.com>
2021-05-05 23:18:31 -0400

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+        officia nostrum placeat ipsum quisquam labore aliquam fuga fugit quam
+        aperiam, temporibus reiciendis. Itaque qui repellat rerum quia quae
+        facere ullam magnam eligendi, quis odio a dolore dolores sit sunt quidem
+        deserunt omnis. Cupiditate consectetur velit rerum ex est et odit.
+      </p>
+    </section>
+  );
+};
+
+export default About;
